60f5272447714387e7862a271ea41524e38d1ec1
Sarthak Agarwal <sarthakagarwal22@gmail.com>
2019-05-05 18:04:56 +0530

    Updates

---
+++ precache-manifest.ad2dd8da4be19dbaf48f1fc1b39b8b02.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc332c901c49ecdd8df8ad509668d59",
+    "url": "/socially/index.html"
+  },
+  {
+    "revision": "1e25d589ec127ec10f33",
+    "url": "/socially/static/css/2.46cdc163.chunk.css"
+  },
+  {
+    "revision": "ab2d4ac206a8cac84bb5",
+    "url": "/socially/static/css/main.11180a3e.chunk.css"
+  },
+  {
+    "revision": "1e25d589ec127ec10f33",
+    "url": "/socially/static/js/2.c79a6198.chunk.js"
+  },
+  {
+    "revision": "ab2d4ac206a8cac84bb5",
+    "url": "/socially/static/js/main.a228bb38.chunk.js"
+  },
+  {
+    "revision": "897552ad30475834a18f",
+    "url": "/socially/static/js/runtime~main.94178b13.js"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/socially/static/media/Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "f1e69e389c1b9f463541c6e2f978d2a3",
+    "url": "/socially/static/media/Italic.f1e69e38.otf"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/socially/static/media/Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/socially/static/media/Regular.6365c40a.otf"
+  },
+  {
+    "revision": "7810fc19ef34c6fe00e31639b2817004",
+    "url": "/socially/static/media/banner-background.7810fc19.png"
+  },
+  {
+    "revision": "5866c2c688e22110ac70e41fcc536b70",
+    "url": "/socially/static/media/delete.5866c2c6.svg"
+  },
+  {
+    "revision": "97e966b0b9f9a710ba1c8deb5ab113ef",
+    "url": "/socially/static/media/edit.97e966b0.svg"
+  },
+  {
+    "revision": "4d9e5a2040325df11d9757258a7c9ba5",
+    "url": "/socially/static/media/email.4d9e5a20.svg"
+  },
+  {
+    "revision": "c0e811d04f821fd1c38bfb8ab0618373",
+    "url": "/socially/static/media/login-right.c0e811d0.jpg"
+  }
+]);
